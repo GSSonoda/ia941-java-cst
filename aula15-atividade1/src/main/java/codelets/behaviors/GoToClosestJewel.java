12b6124e3fd4c8faf8321aa0336b37e610bf8410
Gustavo Sonoda <g.sonoda@sidi.org.br>
2025-06-24 22:02:37 -0300

    Add Goto Jewel in Behaviors

---
+++ aula15-atividade1/src/main/java/codelets/behaviors/GoToClosestJewel.java
@@ -0,0 +1,141 @@
+/*****************************************************************************
+ * Copyright 2007-2015 DCA-FEEC-UNICAMP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    Klaus Raizer, Andre Paraense, Ricardo Ribeiro Gudwin
+ *****************************************************************************/
+
+package codelets.behaviors;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
+import org.json.JSONException;
+import br.unicamp.cst.core.entities.Codelet;
+import br.unicamp.cst.core.entities.Memory;
+import br.unicamp.cst.core.entities.MemoryContainer;
+import br.unicamp.cst.core.entities.MemoryObject;
+import br.unicamp.cst.representation.idea.Idea;
+import ws3dproxy.model.Thing;
+
+public class GoToClosestJewel extends Codelet {
+
+	private Memory closestJewelMO;
+	private Memory selfInfoMO;
+	private MemoryContainer legsMO;
+	private int creatureBasicSpeed;
+	private double reachDistance;
+	private double minimumFuel;
+
+	public GoToClosestJewel(int creatureBasicSpeed, int reachDistance, int minimumFuel) {
+		this.creatureBasicSpeed=creatureBasicSpeed;
+		this.reachDistance=reachDistance;
+                this.name = "GoToClosestJewel";
+		this.minimumFuel = minimumFuel;
+	}
+
+	@Override
+	public void accessMemoryObjects() {
+		closestJewelMO=(MemoryObject)this.getInput("CLOSEST_JEWEL");
+		selfInfoMO=(MemoryObject)this.getInput("INNER");
+		legsMO=(MemoryContainer)this.getOutput("LEGS");
+	}
+
+	@Override
+	public void proc() {
+		// Find distance between creature and closest apple
+		//If far, go towards it
+		//If close, stops
+
+                Thing closestJewel = (Thing) closestJewelMO.getI();
+                Idea cis = (Idea) selfInfoMO.getI();
+
+		if(closestJewel != null)
+		{
+			double jewelX=0;
+			double jewelY=0;
+			try {
+                                jewelX = closestJewel.getCenterPosition().getX();
+                                jewelY = closestJewel.getCenterPosition().getY();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+                        
+			double selfX=(double)cis.get("position.x").getValue();
+			double selfY=(double)cis.get("position.y").getValue();
+			double selfFuel=(double)cis.get("fuel").getValue();
+
+			Point2D pJewel = new Point();
+			pJewel.setLocation(jewelX, jewelY);
+
+			Point2D pSelf = new Point();
+			pSelf.setLocation(selfX, selfY);
+
+			double distance = pSelf.distance(pJewel);
+			//JSONObject message=new JSONObject();
+                        Idea message = Idea.createIdea("message","", Idea.guessType("Property",null,1.0,0.5));
+			try {
+				if(distance>reachDistance && selfFuel >= this.minimumFuel){ //Go to it
+                                        message.add(Idea.createIdea("ACTION","GOTO", Idea.guessType("Property",null,1.0,0.5)));
+                                        message.add(Idea.createIdea("X",(int)jewelX, Idea.guessType("Property",null,1.0,0.5)));
+                                        message.add(Idea.createIdea("Y",(int)jewelY, Idea.guessType("Property",null,1.0,0.5)));
+                                        message.add(Idea.createIdea("SPEED",creatureBasicSpeed, Idea.guessType("Property",null,1.0,0.5)));
+                                        activation=1.0;
+
+				}else{//Stop
+                                        message.add(Idea.createIdea("ACTION","GOTO", Idea.guessType("Property",null,1.0,0.5)));
+                                        message.add(Idea.createIdea("X",(int)jewelX, Idea.guessType("Property",null,1.0,0.5)));
+                                        message.add(Idea.createIdea("Y",(int)jewelY, Idea.guessType("Property",null,1.0,0.5)));
+                                        message.add(Idea.createIdea("SPEED",0, Idea.guessType("Property",null,1.0,0.5)));
+                                        activation=0.5;
+				}
+				legsMO.setI(toJson(message),activation,name);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}	
+		}
+                else {
+                    activation=0.0;
+                    legsMO.setI("",activation,name);
+                }
+                
+	}//end proc
+        
+        @Override
+        public void calculateActivation() {
+        
+        }
+        
+        String toJson(Idea i) {
+            String q = "\"";
+            String out = "{";
+            String val;
+            int ii=0;
+            for (Idea il : i.getL()) {
+                if (il.getL().isEmpty()) {
+                    if (il.isNumber()) val = il.getValue().toString();
+                    else val = q+il.getValue()+q;
+                }
+                else val = toJson(il);
+                if (ii == 0) out += q+il.getName()+q+":"+val;
+                else out += ","+q+il.getName()+q+":"+val;
+                ii++;
+            }
+            out += "}";
+            return out;
+        }
+
+}
